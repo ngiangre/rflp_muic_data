f234cd6b22c0bde6bde5f4d7677ad1f06ab2e550
Nick Giangreco <nick.giangreco@gmail.com>
2025-09-23 21:11:02 -0400

    showing how to get auth

--- dev/dev.R
+++ dev/dev.R
@@ -2,12 +2,3 @@ library(googledrive)
 
 #drive_auth()
 
-drive_download(
-  file = paste0(
-    Sys.getenv("MUIC_GDRIVE_PATH"),
-    "DNA_SR24-58221_C1_neoantigen_class_I_report.xlsx"
-  ),
-  path = "data/DNA_SR24-58221_C1_neoantigen_class_I_report.xlsx"
-)
-
-readxl::read_excel("inst/data/DNA_SR24-58221_C1_neoantigen_class_I_report.xlsx")
